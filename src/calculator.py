29bc8aabbc0729e1ee3e9aceaba02576b0323269
Ivan K <kosi17100@gmail.com>
2025-03-20 15:52:05 +0300

    Calculator done

--- src/calculator.py
+++ src/calculator.py
@@ -9,21 +9,23 @@ from customtkinter import *
 class Calculator:
 
     def __init__(self):
-        self.viewer = CalculatorViewer()
-        self.model = CalculatorModel(self.viewer)
+        self.model = CalculatorModel()
 
     def run(self):
-        self.viewer.root_window.mainloop()
+        self.model.root_window()
 
 
 class CalculatorModel:
 
-    def __init__(self, view: CalculatorViewer):
-        self.__view = view
+    def __init__(self):
+        self.__view = self.CalculatorViewer(self)
         self.activate_second_value = False
         self.first_result = 0
         self.second_result = 0
         self.action = None
+
+    def root_window(self):
+        return self.__view.root_window.mainloop()
 
 
     def handler_data(self, value):
@@ -37,9 +39,11 @@ class CalculatorModel:
             except:
                 self.operation(value)
             return
-        self.second_result = result * 10 + int(value)
-        self.__view.update_output(self.second_result)
-
+        try:
+            self.second_result = result * 10 + int(value)
+            self.__view.update_output(self.second_result)
+        except:
+            self.operation(value)
 
     def operation(self, value):
                 if value == "=":
@@ -68,44 +72,45 @@ class CalculatorModel:
                 return
 
 
-class CalculatorViewer:
+    class CalculatorViewer:
 
-    def __init__(self):
-        self.root_window = self.create_window()
-        self.output_label = None
-        self.btn = None
-        self.button = []
-        self.create_widgets()
+        def __init__(self, CalculatorModel):
+            self.handler_data = CalculatorModel.handler_data
+            self.root_window = self.create_window()
+            self.output_label = None
+            self.btn = None
+            self.button = []
+            self.create_widgets()
 
-    def update_output(self, memory):
-        self.output_label.configure(text=memory)
+        def update_output(self, memory):
+            self.output_label.configure(text=memory)
 
-    def create_window(self):
-        window = CTk()
-        window.geometry("400x200")
+        def create_window(self):
+            window = CTk()
+            window.geometry("400x200")
 
-        return window
+            return window
 
-    def handler_on_click_btn(self):
-        btn_value = self.btn.cget('text')
-        CalculatorModel(self).handler_data(btn_value)
+        def handler_on_click_btn(self, index):
+            btn_value = self.button[index]
+            self.handler_data(btn_value)
 
 
 
-    def create_widgets(self,):
-        vidgets = ["7","8","9","C","4","5","6","%","1","2","3","*","0","=","+","-",]
-        index = 0
+        def create_widgets(self,):
+            vidgets = ["7","8","9","C","4","5","6","/","1","2","3","*","0","=","+","-",]
+            index = 0
 
-        for c in range(3): self.root_window.columnconfigure(index=c, weight=1)
-        for r in range(4): self.root_window.rowconfigure(index=r, weight=1)
+            for c in range(3): self.root_window.columnconfigure(index=c, weight=1)
+            for r in range(4): self.root_window.rowconfigure(index=r, weight=1)
 
-        self.output_label = CTkLabel(self.root_window, text="0")
-        self.output_label.grid(row=0, column=0, columnspan=4)
+            self.output_label = CTkLabel(self.root_window, text="0")
+            self.output_label.grid(row=0, column=0, columnspan=4)
 
-        for i in range(1,5):
-            for j in range(0,4):
-                self.btn = customtkinter.CTkButton(self.root_window, text=vidgets[index],
-                                                   command=lambda btn =self.btn: self.handler_on_click_btn())
-                self.btn.grid(row=i, column=j)
-                self.button.append(self.btn)
-                index += 1
+            for i in range(1,5):
+                for j in range(0,4):
+                    self.btn = customtkinter.CTkButton(self.root_window, text=vidgets[index],
+                                                       command=lambda index = index: self.handler_on_click_btn(index))
+                    self.btn.grid(row=i, column=j)
+                    self.button.append(self.btn.cget("text"))
+                    index += 1
