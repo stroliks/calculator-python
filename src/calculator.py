295ca1ce940b83ace0c9c030abe160ff212ccfab
Ivan K <kosi17100@gmail.com>
2025-03-31 01:19:57 +0300

    с инкапсуляцией, но не работает

--- src/calculator.py
+++ src/calculator.py
@@ -9,107 +9,145 @@ from customtkinter import *
 class Calculator:
 
     def __init__(self):
-        self.model = CalculatorModel()
+        self.__model = CalculatorModel()
 
     def run(self):
-        self.model.root_window()
+        self.__model.root_window()
 
 
 class CalculatorModel:
 
     def __init__(self):
         self.__view = self.CalculatorViewer(self)
-        self.activate_second_value = False
-        self.first_result = 0
-        self.second_result = 0
-        self.action = None
+        self.__activate_second_value = False
+        self.__first_result = 0
+        self.__second_result = 0
+        self.__action = None
+
+    def get_view(self):
+        return self.__view
+
+    def get_first_result(self):
+        return self.__first_result
+
+    def get_second_result(self):
+        return self.__second_result
+
+    def get_action(self):
+        return self.__action
+
+    def set_action(self, value):
+        self.__action = value
+
+    def get_activate_second_value(self):
+        return self.get_activate_second_value
 
     def root_window(self):
-        return self.__view.root_window.mainloop()
+        return self.get_view().get_root_window()
 
+    def set_handler_data(self, value):
+        self.__handler_data(value)
 
-    def handler_data(self, value):
+    def __handler_data(self, value):
 
-        result = int(self.__view.output_label.cget("text"))
-        if not self.activate_second_value:
+        result = int(self.get_view().get_output_label().cget("text"))
+        if not self.get_activate_second_value:
             try:
-                self.first_result = result*10+int(value)
-                self.__view.update_output(self.first_result)
+                self.get_first_result = result * 10 + int(value)
+                self.get_view().set_update_output(self.get_first_result)
 
             except:
-                self.operation(value)
+                self.__operation(value)
             return
         try:
-            self.second_result = result * 10 + int(value)
-            self.__view.update_output(self.second_result)
+            self.get_second_result = result * 10 + int(value)
+            self.get_view().set_update_output(self.get_second_result)
         except:
-            self.operation(value)
+            self.__operation(value)
 
-    def operation(self, value):
+    def __operation(self, value):
                 if value == "=":
-                    if self.action == "*":
-                        view_result = self.first_result * self.second_result
-                    elif self.action == "+":
-                        view_result = self.first_result + self.second_result
-                    elif self.action == "-":
-                        view_result = self.first_result - self.second_result
-                    elif self.action == "/":
-                        view_result = self.first_result / self.second_result
-                    elif self.action == "C":
-                        self.activate_second_value = False
-                        self.second_result = 0
-                        self.first_result = 0
-                        self.action = None
-                        view_result = self.first_result
-                    self.__view.update_output(view_result)
-                    self.first_result = view_result
-                    self.activate_second_value = False
+                    if self.get_action == "*":
+                        view_result = self.get_first_result * self.get_second_result
+                    elif self.get_action == "+":
+                        view_result = self.get_first_result + self.get_second_result
+                    elif self.get_action == "-":
+                        view_result = self.get_first_result - self.get_second_result
+                    elif self.get_action == "/":
+                        view_result = self.get_first_result / self.get_second_result
+                    elif self.get_action == "C":
+                        self.get_activate_second_value = False
+                        self.get_second_result = 0
+                        self.get_first_result = 0
+                        self.set_action(None)
+                        view_result = self.get_first_result
+                    self.get_view().set_update_output(view_result)
+                    self.get_first_result = view_result
+                    self.get_activate_second_value = False
                     return
 
-                self.action = value
-                self.activate_second_value = True
-                self.__view.update_output(0)
+                self.set_action(value)
+                self.get_activate_second_value = True
+                self.get_view().set_update_output(0)
                 return
 
 
     class CalculatorViewer:
 
         def __init__(self, CalculatorModel):
-            self.handler_data = CalculatorModel.handler_data
-            self.root_window = self.create_window()
-            self.output_label = None
+            self.handler_data = CalculatorModel.set_handler_data()
+            self.__root_window = self.__create_window()
+            self.__output_label = None
             self.btn = None
             self.button = []
-            self.create_widgets()
+            self.__create_widgets()
 
-        def update_output(self, memory):
-            self.output_label.configure(text=memory)
+        def get_output_label(self):
+            return self.__output_label
 
-        def create_window(self):
+        def get_button(self):
+            return self.__button
+
+        def __update_output(self, memory):
+            self.__output_label.configure(text=memory)
+
+        def set_update_output(self, memory):
+            return self.__output_label.configure(text=memory)
+
+        def __create_window(self):
             window = CTk()
             window.geometry("400x200")
 
             return window
 
+        def get_root_window(self):
+            return self.__root_window.mainloop()
+
+
         def handler_on_click_btn(self, index):
             btn_value = self.button[index]
+
             self.handler_data(btn_value)
 
+        # @staticmethod
+        # def set_handler_data(value):
+        #     CalculatorModel().set_handler_data(value)
 
 
-        def create_widgets(self,):
+
+        def __create_widgets(self,):
             vidgets = ["7","8","9","C","4","5","6","/","1","2","3","*","0","=","+","-",]
             index = 0
 
-            for c in range(3): self.root_window.columnconfigure(index=c, weight=1)
-            for r in range(4): self.root_window.rowconfigure(index=r, weight=1)
+            for c in range(3): self.__root_window.columnconfigure(index=c, weight=1)
+            for r in range(4): self.__root_window.rowconfigure(index=r, weight=1)
 
-            self.output_label = CTkLabel(self.root_window, text="0")
-            self.output_label.grid(row=0, column=0, columnspan=4)
+            self.__output_label = CTkLabel(self.__root_window, text="0")
+            self.__output_label.grid(row=0, column=0, columnspan=4)
 
             for i in range(1,5):
                 for j in range(0,4):
-                    self.btn = customtkinter.CTkButton(self.root_window, text=vidgets[index],
+                    self.btn = customtkinter.CTkButton(self.__root_window, text=vidgets[index],
                                                        command=lambda index = index: self.handler_on_click_btn(index))
                     self.btn.grid(row=i, column=j)
                     self.button.append(self.btn.cget("text"))
